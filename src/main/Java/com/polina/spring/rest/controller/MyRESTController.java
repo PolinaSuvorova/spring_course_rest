9947c2ff90272380b29b684f38dcff79ea593cbd
PolinaSuvorova <polina.tatara@gmail.com>
2024-02-26 16:17:39 +0300

    add ExceptionHandlers

--- src/main/Java/com/polina/spring/rest/controller/MyRESTController.java
+++ src/main/Java/com/polina/spring/rest/controller/MyRESTController.java
@@ -1,8 +1,12 @@
 package com.polina.spring.rest.controller;
 
 import com.polina.spring.rest.entity.Employee;
+import com.polina.spring.rest.exception.EmployeeIncorrectData;
+import com.polina.spring.rest.exception.NoSuchEmployeeException;
 import com.polina.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,10 +25,29 @@ public class MyRESTController {
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable("id") int id) {
-        return employeeService.getEmployee(id);
+        Employee employee =  employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id= " + id);
+        }
+        return employee;
+
     }
     @DeleteMapping("/employees/{id}")
     public void deleteEmployee(@PathVariable("id") int id) {
          employeeService.deleteEmployee(id);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return  new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return  new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
